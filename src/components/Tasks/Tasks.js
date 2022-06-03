937ae60c9acb5c27996b36c4f20ee7a222ef98dc
Doniyor Azizov <DAzizov@inno.tech>
2022-06-03 18:21:37 +0500

    функционал добавлнеия задач

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 
-
-export const Tasks = ({ onSubmit }) => {
+export const Tasks = ({ onSubmit, categoryId }) => {
   const [taskName, setTask] = useState('');
 
   const handleOnChange = ({ target }) => setTask(target.value);
   const handleOnSubmit = (event) => {
     event.preventDefault();
 
-    onSubmit(taskName);
+    onSubmit({ taskName, categoryId });
     setTask('');
   };
   return (
-    <form className='Tasks'  onSubmit={handleOnSubmit}>
+    <form className="Tasks" onSubmit={handleOnSubmit}>
       <input className="in" type="text" value={taskName} onChange={handleOnChange} />
       <button className="btn" type="button" onClick={handleOnSubmit}>
         Добавить задачу
